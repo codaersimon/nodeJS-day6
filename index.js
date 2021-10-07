66b5352fe72e424451b62bb41064495c77860866
codaersimon <codaersimon@gmail.com>
2021-10-07 09:16:03 +0100

    addedUser

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const app = express();
+require('dotenv').config()
+console.log(process.env)
 // 
 const port = process.env.PORT || 3000
 
 
-app.set('views', './views')
-//view engine definieren
-app.set('view engine', 'ejs')
+// app.set('views', './views')
+// view engine definieren
+// app.set('view engine', 'ejs')
 
 app.listen(port,() =>{
     console.log(`listening at https://localhost:${port}`)
 })
 
 app.get('/',(req,res) =>{
-    res.render('index.ejs',{title: 'homepage'})
+    res.render('index.ejs',{title: 'home', apiKey: process.env.API_KEY, username: process.env.USERNAME})
 })
 
 app.get('/about',(req,res) =>{
